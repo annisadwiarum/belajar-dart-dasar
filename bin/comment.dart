7f99830715d4ad345546224a278824c6de7d0991
Annisa Dwi Arum <annisadwiarum710@gmail.com>
2026-01-24 01:00:26 +0700

    update

---
+++ bin/comment.dart
@@ -0,0 +1,12 @@
+/// ini cara komentar untuk dokumentasi file comment.dart
+
+void main() {
+    // ini komentar 1 baris variabel name
+    var name = "annisa";
+    print(name);
+
+    /* ini komentar untuk beberapa baris kode
+    hidup jok***!
+    heyy ant tech ant tech async!
+    */
+}
---
+++ bin/number.dart
@@ -0,0 +1,10 @@
+void main () {
+// number memiliki 2 tipe data utama yaitu integer dan double
+// tipe data integer = bilangan bulat    
+    int number1 = 10;
+    print(number1);
+
+// tipe data double = bilangan desimal
+    double number2 = 10.5;
+    print(number2);
+}
